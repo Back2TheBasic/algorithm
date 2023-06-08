ff2eaec017e700863fd3c1e3cdea0a457cbd9d78
Bong <borobong230@gmail.com>
2023-06-08 22:44:35 +0900

    feat: Add 11399 bong's code

--- greedy/11399/11399_sangbong.js
+++ greedy/11399/11399_sangbong.js
@@ -19,3 +19,29 @@ const { result } = input
   .reduce(add, { result: 0, beforeTime: 0 });
 
 console.log(result);
+
+// const input = require("fs")
+//   .readFileSync("dev/stdin")
+//   .toString()
+//   .trim()
+//   .split("\n")
+//   .splice(1)[0]
+//   .split(" ")
+//   .map(Number);
+
+// let numbers = input.sort((a, b) => {
+//   return a - b;
+// });
+
+// let result = 0;
+// let beforeTime = 0;
+// const add = value => {
+//   result += beforeTime + value;
+//   beforeTime += value;
+// };
+
+// for (let i of numbers) {
+//   add(i);
+// }
+
+// console.log(result);
