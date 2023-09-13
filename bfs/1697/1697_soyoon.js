3f68fc98eb500da07531acb6871f1ee9f1cfe663
Soyoon Jeong <sytkfkd96@gmail.com>
2023-09-13 09:32:23 +0900

    [algorithm]: soyoon bfs

---
+++ bfs/1697/1697_soyoon.js
@@ -0,0 +1,39 @@
+let fs = require('fs');
+let input = fs.readFileSync('./dev/stdin').toString().split('\n');
+
+const MAX = 100001;
+let [n, k] = input[0].split(' ').map(Number); // n: 초기위치, k: 동생의 위치
+let visited = Array(MAX).fill(0);
+
+class Queue {
+  constructor() {
+    this._arr = [];
+  }
+  enqueue(item) {
+    this._arr.push(item);
+  }
+  dequeue() {
+    return this._arr.shift();
+  }
+}
+
+function bfs() {
+  queue = new Queue();
+  queue.enqueue(n);
+
+  while (queue.getLength !== 0) {
+    let cur = queue.dequeue();
+
+    if (cur === k) return visited[cur];
+
+    for (let nxt of [cur - 1, cur + 1, cur * 2]) {
+      if (nxt < 0 || nxt >= MAX) continue;
+      if (visited[nxt] === 0) {
+        visited[nxt] = visited[cur] + 1;
+        queue.enqueue(nxt);
+      }
+    }
+  }
+}
+
+console.log(bfs());
